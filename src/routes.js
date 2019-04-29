e13c9333cea49027b6c8fd797b7d24912b3d39ca
Victor Manoel <victormsilva2001@gmail.com>
2019-04-29 12:19:45 -0300

    Set routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // Pages
 import Home from "./pages/home";
+import Video from "./pages/video";
 import NotFound from "./pages/notFound";
 
+// Routes
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route path="/watch/:id" component={Video} exact />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
