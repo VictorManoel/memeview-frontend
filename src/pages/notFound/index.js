16742eb60cc199b4a9b0f3573f22d8cecb17db30
Victor Manoel <victormsilva2001@gmail.com>
2019-04-30 15:37:59 -0300

    Not found page design

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
-const NotFound = () => <h1>404</h1>;
+const NotFound = () => (
+  <main id="notfound">
+    <h1>404</h1>
+    <h2>Página não encontrada!</h2>
+    <Link to="/" className="back">
+      Voltar
+    </Link>
+  </main>
+);
 
 export default NotFound;
