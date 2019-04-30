9dc7497978b18b340e1892861d5d0449d76b3307
Victor Manoel <victormsilva2001@gmail.com>
2019-04-29 21:46:57 -0300

    Home

--- src/pages/home/list/item/index.js
+++ src/pages/home/list/item/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Item = () => (
   <article className="item">
-    <header className="thumbnail" />
-    <div className="details" />
+    <Link to="watch/teste" className="item-link">
+      <header className="thumbnail" title="Vídeo">
+        <img src="https://picsum.photos/320/180" alt="imagem" />
+      </header>
+      <div className="item-details">
+        <h2 title="Vídeo">Lorem Ipsum</h2>
+        <span>Há 2 minutos</span>
+      </div>
+    </Link>
   </article>
 );
 
