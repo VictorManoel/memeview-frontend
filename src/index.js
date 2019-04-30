56ca3d0a9df5a31a0225a99f32c61f3800c484bf
Victor Manoel <victormsilva2001@gmail.com>
2019-04-30 19:20:38 -0300

    Loading page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
 import * as serviceWorker from "./serviceWorker";
-import "./styles.css";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
 
